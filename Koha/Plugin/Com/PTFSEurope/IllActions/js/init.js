580cbacae487a9c4aa030cfa792b60532139bcb3
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2024-03-27 14:54:20 -0100

    address csrf changes compatibility

--- Koha/Plugin/Com/PTFSEurope/IllActions/js/init.js
+++ Koha/Plugin/Com/PTFSEurope/IllActions/js/init.js
@@ -4,9 +4,12 @@ const create_form_branchcode_input = $("#create_form #branchcode");
 const is_create_page =
   window.location.href.indexOf(
     "/cgi-bin/koha/ill/ill-requests.pl?method=create"
-  ) > -1;
+  ) > -1 ||
+  window.location.href.indexOf("/cgi-bin/koha/ill/ill-requests.pl?op=create") >
+    -1;
 
 const is_illview_page =
-  window.location.href.indexOf(
-    "/cgi-bin/koha/ill/ill-requests.pl?method=illview"
-  ) > -1;
+  window.location.href.indexOf("/cgi-bin/koha/ill/ill-requests.pl?op=illview") >
+    -1 ||
+  window.location.href.indexOf("/cgi-bin/koha/ill/ill-requests.pl?op=illview") >
+    -1;
