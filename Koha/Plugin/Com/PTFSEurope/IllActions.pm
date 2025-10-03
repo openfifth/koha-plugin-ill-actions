7f58ecb31556c7d0739e3e00a893d8668b0842f1
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2025-10-03 11:41:28 +0000

    version bump

--- Koha/Plugin/Com/PTFSEurope/IllActions.pm
+++ Koha/Plugin/Com/PTFSEurope/IllActions.pm
@@ -19,13 +19,13 @@ use Koha::Patrons;
 use Koha::Patron::Attribute::Types;
 use Koha::Patron::Categories;
 
-our $VERSION = "2.4.7";
+our $VERSION = "2.5.0";
 
 our $metadata = {
     name            => 'IllActions',
     author          => 'Open Fifth',
     date_authored   => '2023-10-30',
-    date_updated    => '2025-09-08',
+    date_updated    => '2025-10-03',
     minimum_version => '25.05.00.000',
     maximum_version => undef,
     version         => $VERSION,
