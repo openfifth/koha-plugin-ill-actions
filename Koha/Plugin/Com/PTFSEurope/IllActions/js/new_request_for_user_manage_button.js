7419c04aec6dafb79f04f321fcf292556b8d1004
Pedro Amorim <pedro.amorim@openfifth.co.uk>
2026-01-07 10:10:09 -0100

    Move 'New request for this user' button to 2nd place
    This ensures harmony with bug 41536.

--- Koha/Plugin/Com/PTFSEurope/IllActions/js/new_request_for_user_manage_button.js
+++ Koha/Plugin/Com/PTFSEurope/IllActions/js/new_request_for_user_manage_button.js
@@ -4,8 +4,8 @@ if (is_illview_page) {
     const el =
       '<a title="New request for this user" class="btn btn-default" href="/api/v1/contrib/ill_actions/new_request_for_patron/' +
       searchParams.get("illrequest_id") +
-      '"> <span class="fa fa-plus"></span>New request for this user</a>';
+      '"><span class="fa fa-plus"></span> New request for this user</a>';
 
-    $("#request-toolbar").prepend(el);
+    $(el).insertAfter("#request-toolbar > :first-child");
   }
 }
