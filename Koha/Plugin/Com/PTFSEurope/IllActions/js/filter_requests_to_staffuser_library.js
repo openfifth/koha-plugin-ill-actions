ee36d89a9c5bb08ff4da5b9182dfba1649440769
Olivia Reynolds <olivia.reynolds@openfifth.co.uk>
2026-07-15 11:43:05 +0100

    Allow initially filtering ILL requests table to user's ILL group
    Depends on Koha Bug 43089, but shouldn't error without it.

--- Koha/Plugin/Com/PTFSEurope/IllActions/js/filter_requests_to_staffuser_library.js
+++ Koha/Plugin/Com/PTFSEurope/IllActions/js/filter_requests_to_staffuser_library.js
@@ -1,9 +1,17 @@
 if (is_table_page) {
-  var library = $(
-    "#logged-in-info-full > strong > span.logged-in-branch-name"
-  ).text();
-  $("#illfilter_branchname option:contains(" + library + ")").attr(
-    "selected",
-    "selected"
-  );
+    if(group_branchcodes && Object.keys(group_branchcodes).length > 0 && $("#illfilter_group").length > 0) {
+        // Select the group with the most constituent libraries
+        const largest_group = Object.keys(group_branchcodes)
+              .toSorted((x, y) => group_branchcodes[y].length - group_branchcodes[x].length)
+        [0];
+        $("#illfilter_group").val(largest_group);
+    } else {
+        var library = $(
+          "#logged-in-info-full > strong > span.logged-in-branch-name"
+        ).text();
+        $("#illfilter_branchname option:contains(" + library + ")").attr(
+          "selected",
+          "selected"
+        );
+    }
 }
